17db2284355e5e4f37d8a57e521177aabe569e58
2impaoo <baoga271104@gmail.com>
2026-01-05 17:53:15 +0700

    Remove app folder

--- app/lib/bloc/settings/settings_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class SettingsCubit extends Cubit<bool> {
-  // Trạng thái true: Hiển thị số dư
-  // Trạng thái false: Ẩn số dư (******)
-  SettingsCubit() : super(true);
-
-  void toggleBalanceVisibility() => emit(!state);
-}
